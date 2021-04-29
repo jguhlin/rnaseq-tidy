1fe3ff208aca353438337240a3b62d5880f20304
Joseph Guhlin <joseph.guhlin@gmail.com>
2021-04-29 12:07:50 +1200

    Fix paths for Rscript

--- 0_analyses/analysis.R
+++ 0_analyses/analysis.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 # Let’s look at our dataset and perform some basic checks before we do a differential expression analysis.
 
-fcData <- read.table('yeast_counts_all_chr.txt', sep='\t', header=TRUE)
+fcData <- read.table('../data/yeast_counts_all_chr.txt', sep='\t', header=TRUE)
 head(fcData)
 
 # check dimentions
@@ -20,7 +20,7 @@ rownames(counts) <- fcData$Geneid
 head(fcData)
 
 # create a plot of counts per gene by sample
-png(filename = "figure1_counts.png")
+png(filename = "../output/figure1_counts.png")
 boxplot(as.matrix(counts) ~ col(counts), main = "Counts per Gene", xlab = "Strain", ylab = "Count")
 dev.off()
 
@@ -32,6 +32,6 @@ colSums(counts == 0)
 logCounts <- log2(as.matrix(counts) + 0.5)
 
 # Now we can see the per-sample distributions more clearly:
-png(filename = "figure2_logCounts.png")
+png(filename = "../output/figure2_logCounts.png")
 boxplot(as.matrix(logCounts) ~ col(logCounts), main = "Log Transformed Counts", xlab = "Strain", ylab = "log2(Count)")
 dev.off()
